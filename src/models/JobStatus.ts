ae86905ecfec7c8fd7e276ce03c9d031a2ed9ee4
Vedant Mahadik <mahadikvedant32@gmail.com>
2024-11-16 01:30:56 +0530

    minor fixes

--- src/models/JobStatus.ts
+++ src/models/JobStatus.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const JobStatusSchema = z.object({
   id: z.number(),
   flow_id: z.number(),
-  timestamp: z.string(),
+  created_at: z.string(),
   status: z.string(),
   details: z.string(),
 });
@@ -13,7 +13,7 @@ export type JobStatus = z.infer<typeof JobStatusSchema>;
 export const createJobStatus = (jobStatus: JobStatus) => {
   return Object.freeze({
     flow_id: jobStatus.flow_id,
-    timestamp: jobStatus.timestamp,
+    created_at: jobStatus.created_at,
     status: jobStatus.status,
     details: jobStatus.details,
   });
